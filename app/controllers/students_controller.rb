f4564e2210d7a06f2438040b6853b2edae9cfade
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-02-09 18:10:52 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,32 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+
+    # @student = Student.create(frist_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student = Student.create(params[:student])
+    redirect_to student_path(@student)
+
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params[:student])
+    redirect_to student_path(@student)
+  end
+
+end #end of class
